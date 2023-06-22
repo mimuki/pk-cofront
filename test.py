10193c24983a625c0d6fe9ed4ee175945638938d
mimuki <60542090+mimuki@users.noreply.github.com>
2023-06-22 09:30:45 +0930

    Member creation working, yay

--- test.py
+++ test.py
@@ -1,47 +1,61 @@
-from pluralkit import Client
+RED = '\033[91m'
+GREEN = '\033[92m'
+CYAN = '\033[96m'
+RESET = '\033[0m'
 
+comboMembers = [] # The member objects of each member to combine
+names = [] # The names of each member to combine
+nicks = [] # The display names of each member to combine
+ids = [] # The IDs of each member to combine
+
+
+from pluralkit import Client, Privacy, AutoproxyMode
 # keep token not in this file, for obvious reasons
-from config import TOKEN
+try:
+    from config import TOKEN, MODE
+except ModuleNotFoundError:
+    print(f"{RED}Could not find config.py.{RESET}")
+    config = open("config.py", "w")
+    config.write("TOKEN = '' # get yours by running `pk;token` in Discord\nMODE = 'autoproxy' # options: 'autoproxy', 'switch' ")
+    config.close()
+    print("You need to manually edit that file, and put your token in it.")
+    exit()
 pk = Client(TOKEN, async_mode=False)
 
 system = pk.get_system()
-print(f"Hi, {system.name}!")
-
+if system.name == None:
+    print("Connected successfully!")
+else:
+    print(f"Hi, {CYAN}{system.name}{RESET}!")
+print("This is a tool that combines members into a single member,")
+print("so you can proxy as all of them.\n")
 # Ask for combo members
-inputMembers = input("Write the names or IDs of who you'd like to combo: ")
-# Lowercase it, so that it's not case sensitive
-inputMembers = inputMembers.lower()
-# Turn it into a list, split by spaces
-# This breaks multi word names, but those suck anyway
-inputMembers = inputMembers.split(' ')
+inputMembers = input("Enter names of members to combine: ").lower().split(' ')
 # remove extra strings from stuff like double spaces
 inputMembers = list(filter(None, inputMembers))
 
-comboMembers = [] 
-names = [] # For member making
-nicks = [] # For display name making
-print(f"Thanks.\nI'm going to try and combine these members:")
+print("Thanks.\nI'm going to try and combine these members:")
 for member in inputMembers:
-    print(f"- {member}")
+    print(f"- {CYAN}{member}{RESET}")
 # Actually get your system list from pk
 members = pk.get_members()
 
 for inputMember in inputMembers:
-    print(f"Searching for \033[96m{inputMember} \033[0min your system.")
-    print(f"{(inputMembers.index(inputMember))} of {len(inputMembers)} members processed so far.")
+    print(f"Searching for {CYAN}{inputMember}{RESET} in your system.")
+    print(f"{CYAN}{(inputMembers.index(inputMember))}{RESET} of {CYAN}{len(inputMembers)}{RESET} members processed so far.")
     found = False
     for member in members:
         if inputMember == member.name.lower():
-            print(f"\033[32mSuccess!\033[0m Found \033[36m{inputMember}\033[0m in system list.")
+            print(f"{GREEN}Success!{RESET} Found {CYAN}{inputMember}{RESET} in system list.")
             comboMembers.append(member)
             found = True
     if not found:
-        print(f"Couldn't find {inputMember}.")
-        break
+        print(f"{RED}Couldn't find {inputMember}.{RESET}")
+        exit()
 
 print("Input processed! I'm going to combine these members:")
 for member in comboMembers:
-    print(f"- {member.name} (nickname: {member.display_name})")
+    print(f"- {CYAN}{member.name}{RESET} (nickname: {CYAN}{member.display_name}{RESET})")
     names.append(member.name)
     if member.display_name:
         nicks.append(member.display_name)
@@ -49,23 +63,50 @@ for member in comboMembers:
         nicks.append(member.name)
 
 comboName = "+".join(names)
+comboNameDesc = "`\n`pk;m ".join(names)
 comboNick = "・".join(nicks)
-print(system.tag)
+
+# If it would break PK (doesn't check server tag)
 if len(comboNick) + len(system.tag) + 1 > 80 or len(comboName) > 80:
-    print("This combination's name or display name is too long for Pluralkit.")
-    print(f"The combo member's name has {len(comboName)} characters, their display name has {len(comboNick)} characters, and your system tag has {(len(system.tag)+1)} characters.")
-    print(f"The max total webhook length is 80 characters, but you have {(len(comboNick) + len(system.tag) +1)}. Your name length is {len(comboName)}.")
-else:
+    print("{RED}This combination's name or display name is too long for Pluralkit.{RESET}")
+    print(f"Member name length: {len(comboName)} characters")
+    print(f"Display name length: {len(comboNick)} characters")
+    print(f"System tag length: {(len(system.tag) +1)}")
+    print(f"Combined webhook name length: {(len(comboNick) + 1 + len(system.tag))}")
+    print("\nOne of these is above the limit of 80 characters.")
+    exit()
+else: # Should be fine
     print("Their name when sending messages is:")
-    print(comboNick)
+    print(f"- {CYAN}{comboNick}{RESET}")
     print("Their name for running commands is:")
-    print(comboName)
+    print(f"- {CYAN}{comboName}{RESET}")
     found = False
     for member in members:
         if member.name == comboName:
-            print(f"{member.name} already has a member!")
+            print(f"Combo member {CYAN}{member.name}{RESET} already exists in pluralkit.")
+            comboMember = member
             found = True
     if found == False:
-        print("You don't have a PK member yet")
+        desc = f'This is the combination of multiple seperate members into a single Pluralkit proxy.\n\nFor more information, see:\n`pk;m {comboNameDesc}`'
+        print(f"Combo member {CYAN}{comboName}{RESET} doesn't exist in pluralkit, let me make them for you!")
+        comboMember = pk.new_member(
+                comboName, 
+                display_name=comboNick, 
+                description=desc, 
+                visibility=Privacy.PRIVATE,
+                )
+        print(f"Created {CYAN}{comboName}{RESET} successfully!")
+    if MODE == 'autoproxy':
+        print(f"Autoproxy mode selected. This works like running pk;ap {comboName} does in Discord.")
+        print(f"Because I'm not discord, I need you to tell me what server you want to autoproxy in. (If you don't want to run this for every server you're in, consider trying {CYAN}MODE = 'switch'{RESET} in config.py.)")
+        guild = input("Enter the server ID for the server you want to proxy in: ")
+        pk.update_autoproxy_settings(guild, AutoproxyMode.MEMBER, autoproxy_member=comboMember)
+    elif MODE == 'switch':
+        print(f"Switching in {CYAN}{comboName}{RESET}...")
+        pk.new_switch(comboMember)
+        print(f"{GREEN}Switch successful!{RESET} Make sure to run {CYAN}pk;ap front{RESET} in any servers you want to autoproxy in, if you haven't already.")
     else:
-        print("You already have a member!")
+        print(f"{RED}Invalid mode selected.{RESET}")
+        print(f"Check your config.py; the only options I take are {CYAN}MODE = 'autoproxy'{RESET}")
+        print(f"and {CYAN}MODE = 'switch'{RESET}\n")
+        exit()
